c21320e3c853cdf7babc52d4854d0527ba114a58
kmss6905 <kmss69052@naver.com>
2022-05-22 12:51:49 +0900

    DDD pratice init

---
+++ src/main/java/com/example/dddshopping/DddShoppingApplication.java
@@ -0,0 +1,13 @@
+package com.example.dddshopping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DddShoppingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DddShoppingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dddshopping/DddShoppingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dddshopping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DddShoppingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
