653212056d215217bf05d02a51cadd2879dba6a6
kmss6905 <kmss69052@naver.com>
2022-05-24 23:58:48 +0900

    도매인 모델 도출 방법

--- src/main/java/com/example/dddshopping/order/Order.java
+++ src/main/java/com/example/dddshopping/order/Order.java
@@ -1,14 +1,23 @@
 package com.example.dddshopping.order;
 
+// 주문 도메인
 public class Order {
     private OrderState state;
     private ShippingInfo shoppingInfo;
     private PaymentInfo paymentInfo;
 
+    // Order -> ShippingInfo
     public void changeShipping(ShippingInfo newShipping){
         if(!state.isShippingChangeable()){
             throw new IllegalArgumentException("can't change shipping in " + state);
         }
         this.shoppingInfo = newShipping;
     }
+
+    // Order -> OrderState
+    private boolean isShippingChangeable(){
+        return state == OrderState.PAYMENT_WAITING || state == OrderState.PREPARING;
+    }
+
+
 }
--- src/main/java/com/example/dddshopping/order/OrderState.java
+++ src/main/java/com/example/dddshopping/order/OrderState.java
@@ -1,5 +1,10 @@
 package com.example.dddshopping.order;
 
+/**
+ * 도메인 : 소프트웨어로 해결하고자 하는 문제 영역
+ * 주문 대기중(PAYMENT_WAITING) 상태와 상품 준비 중(PREPARING) 상태의 메서드는 true를 리턴함
+ * 즉, OrderState 는 주문 대기 중이거나 상품 준비 중에는 배송지를 변경할 수 있다는 도메인 규칙을 구현하고 있음
+ */
 public enum OrderState {
     PAYMENT_WAITING{
         public boolean isShippingChangeable(){
